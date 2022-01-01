71152a82cbc5d01147dec0c02a8d87a1b5d53db9
BUNNY <18268893871@163.com>
2022-01-01 16:38:12 +0800

    lab2debug

--- lab2/DebugPractice/DebugExercise2.java
+++ lab2/DebugPractice/DebugExercise2.java
@@ -12,7 +12,7 @@ public class DebugExercise2 {
            step out button because you're not going to learn anything. */
         int z = ~(b - a) >> 31;
 
-        int max = b & w | a & z;
+        int max = b & z | a & w;
         return max;
     }
 
@@ -57,10 +57,15 @@ public class DebugExercise2 {
 
     /** Returns the sum of all elements in x. */
     public static int arraySum(int[] x) {
+
+        if (x == null){
+            return 0;
+        }
+
         int i = 0;
         int sum = 0;
         while (i < x.length) {
-            sum = sum + add(sum, x[i]);
+            sum = add(sum, x[i]);
             i = i + 1;
         }
         return sum;
