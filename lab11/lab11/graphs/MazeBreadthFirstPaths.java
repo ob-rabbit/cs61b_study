81ed33c09bfdba08a5f462b10e4617a4a1c31e53
BUNNY <18268893871@163.com>
2022-01-19 20:09:04 +0800

    lab11 广度优先搜索

--- lab11/lab11/graphs/MazeBreadthFirstPaths.java
+++ lab11/lab11/graphs/MazeBreadthFirstPaths.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeBreadthFirstPaths extends MazeExplorer {
     /* Inherits public fields:
@@ -9,21 +11,54 @@ public class MazeBreadthFirstPaths extends MazeExplorer {
     public int[] edgeTo;
     public boolean[] marked;
     */
+    private int s;
+    private int t;
+    private boolean targetFound = false;
+    private Maze maze;
+
 
     public MazeBreadthFirstPaths(Maze m, int sourceX, int sourceY, int targetX, int targetY) {
         super(m);
-        // Add more variables here!
+        maze = m;
+        s = maze.xyTo1D(sourceX, sourceY);
+        t = maze.xyTo1D(targetX, targetY);
+        distTo[s] = 0;
+        edgeTo[s] = s;
     }
 
-    /** Conducts a breadth first search of the maze starting at the source. */
+    /**
+     * Conducts a breadth first search of the maze starting at the source.
+     */
     private void bfs() {
-        // TODO: Your code here. Don't forget to update distTo, edgeTo, and marked, as well as call announce()
+        LinkedList<Integer> queue = new LinkedList<>();
+        marked[s] = true;
+        announce();
+        queue.addLast(s);
+        Integer curNode = s;
+
+        while (!queue.isEmpty()) {
+            curNode = queue.peekFirst();
+            queue.pop();
+            marked[curNode] = true;
+            announce();
+            if (curNode == t) {
+                break;
+            }
+            for (int w : maze.adj(curNode)) {
+                if (!marked[w]) {
+                    edgeTo[w] = curNode;
+                    announce();
+                    distTo[w] = distTo[curNode] + 1;
+                    queue.addLast(w);
+                }
+            }
+        }
     }
 
 
     @Override
     public void solve() {
-        // bfs();
+        bfs();
     }
 }
 
