1828546450f7db992ce8ead944563ff1a1ef153d
BUNNY <18268893871@163.com>
2022-01-19 21:28:43 +0800

    lab11 dfs判断环

--- lab11/lab11/graphs/MazeCycles.java
+++ lab11/lab11/graphs/MazeCycles.java
@@ -1,7 +1,9 @@
 package lab11.graphs;
 
+import java.util.LinkedList;
+
 /**
- *  @author Josh Hug
+ * @author Josh Hug
  */
 public class MazeCycles extends MazeExplorer {
     /* Inherits public fields:
@@ -10,15 +12,81 @@ public class MazeCycles extends MazeExplorer {
     public boolean[] marked;
     */
 
+    private int s;
+    private int t;
+    private Maze maze;
+
     public MazeCycles(Maze m) {
         super(m);
+        maze = m;
+        s = maze.xyTo1D(1, 1);
+        t = maze.xyTo1D(m.N(), m.N());
+        distTo[s] = 0;
     }
 
     @Override
     public void solve() {
-        // TODO: Your code here!
+        cyclesCheckByDFS(0);
+
     }
 
     // Helper methods go here
+
+    private void cyclesCheckByDFS(int v) {
+        LinkedList<Integer> stack = new LinkedList<>();
+
+        marked[v] = true;
+        announce();
+
+        stack.addLast(v);
+        Integer curNode;
+        Integer parentNode = -1;
+        int flag = 0;
+        while (!stack.isEmpty()) {
+            flag = 0;
+            curNode = stack.peekLast();
+            if (curNode == t) {
+                return;
+            }
+            for (int w : maze.adj(curNode)) {
+
+                if (!marked[w]) {
+                    marked[w] = true;
+                    distTo[w] = distTo[curNode] + 1;
+                    announce();
+                    parentNode = curNode;
+                    int cycleStart = checkCycles(w, parentNode);
+                    if (cycleStart != -1) {
+                        int node;
+                        int cur = w;
+                        while ((node = stack.removeLast()) != cycleStart) {
+                            edgeTo[cur] = node;
+                            announce();
+                            cur = node;
+                        }
+                        edgeTo[cur] = node;
+                        edgeTo[cycleStart] = w;
+                        announce();
+                        return;
+                    }
+                    stack.addLast(w);
+                    flag = 1;
+                    break;
+                }
+            }
+            if (flag == 0) {
+                stack.removeLast();
+            }
+        }
+    }
+
+    private int checkCycles(int curNode, Integer parentNode) {
+        for (int w : maze.adj(curNode)) {
+            if (w != parentNode && marked[w]) {
+                return w;
+            }
+        }
+        return -1;
+    }
 }
 
